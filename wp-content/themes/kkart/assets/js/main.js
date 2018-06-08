0841783a1166051303b5ff806830454668df7894
Erin Kahn <erinnicolekahn@gmail.com>
2018-06-08 12:08:30 -0400

    fixed the jumpy hamburger

--- wp-content/themes/kkart/assets/js/main.js
+++ wp-content/themes/kkart/assets/js/main.js
@@ -7,5 +7,6 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     console.log('burger has been clicked');
     document.querySelector('.hamburger').classList.toggle('open');
     document.querySelector('.hamburger').classList.toggle('mobileMenu');
+    document.querySelector('.logoMobile').classList.toggle('mobileMenu');
     document.querySelector('#menu-nav-menu').classList.toggle('show');
 })
