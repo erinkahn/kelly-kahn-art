1e3e1188bd88dc4f23dc79ce7f4c64fd0419065b
Erin Kahn <erinnicolekahn@gmail.com>
2018-06-25 19:22:51 -0400

    fixed the archive page

--- wp-content/themes/kkart/archive-portfolio.php
+++ wp-content/themes/kkart/archive-portfolio.php
@@ -2,34 +2,36 @@
 	
 <section id="archive-page" role="main">
 
-	<h1>Portfolio archive!</h1>
-
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<!-- portfolio content -->
 	
 	<div class="categories">
 
-		<div class="portfolio-categories">
-			
 		
-			<div class="single-post"> <!-- four images portfolios -->
-				<a class="post-link" href="<?php the_permalink(); ?>"> <!-- link to this post-->
-					<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-					<h3><?php the_title();?></h3>
-				</a>
-				
+			
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+			<div class="portfolio-categories">
+		
+				<div class="single-post"> <!-- four images portfolios -->
+					<a class="post-link" href="<?php the_permalink(); ?>"> <!-- link to this post-->
+						<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+						<h3><?php the_title();?></h3>
+					</a>
+					
+				</div>
+
 			</div>
 
-		</div>
+		<?php endwhile; endif; ?>	
 
 	</div>
 			
 		
-	<?php endwhile; endif; ?>
 
 
-	<div class="contactForm">
+
+	<div class="contactForm archive">
 		<div class="form-text">
 			<p>Have you been wanting a new portrait of your family to hang up in your home? Do you want to improve your art skills by taking a private art class?</p>
 			<h1>Contact Kelly.</h1>
