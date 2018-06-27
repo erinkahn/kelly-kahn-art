dec569c1a35c6c379286f436801efd7bd0db596f
Erin Kahn <erinnicolekahn@gmail.com>
2018-06-27 17:11:55 -0400

    about page

--- wp-content/themes/kkart/page-about.php
+++ wp-content/themes/kkart/page-about.php
@@ -10,18 +10,18 @@ Template Name: Page About
 <section class="about">
 
 
+    <div class="kelly">
+        <img src="<?php the_field('about_image');?>" alt="">
+    </div>
 
-    <h3>About the artist</h3>
-    <p>Kelly is a Birmingham-based artist, specializing in portraits, landscapes, and still-lifes. 
-    Hailing from Albertville, in northeast Alabama, she has spent her life expressing herself through art – from 
-    copying art out of children’s books while she was still a child herself, to illustrating cookbooks as a teenager. 
-    The natural progression was to follow her heart to a career in art.</p>
-    <p>She received her BA in Interior Design from Auburn University in 1983 and went on to become a registered interior
-    designer in Birmingham. Believing that art can be learned, she’s been teaching fine art and painting since 1999 
-    to budding artists of all ages. She has even been commissioned to paint pieces for a variety of organizations including 
-    Women’s Club Directories, Indian Springs School landscapes, and portraits of distinguished Alabamians.</p>
+    <div class="aboutText">
+        <h3><?php the_field('about_header');?></h3>
+        <p><?php the_field('about_text');?></p>
+        <p><?php the_field('about_text2');?></p>
+    </div>
+    
 
-    <h3>Influences</h3>
+    <!-- <h3>Influences</h3>
     <ul>
         <li>Jan Kunz</li>
         <li>Winslow Homer</li>
@@ -30,10 +30,10 @@ Template Name: Page About
         <li>Rollina Ogelsbay</li>
         <li>John Lonergan</li>
         <li>Al Sella</li>
-    </ul>
+    </ul> -->
 
 
-    <h3>Exhibitions and Galleries</h3>
+    <!-- <h3>Exhibitions and Galleries</h3>
     <ul>
         <li>Vestavia Art Association guest artist/speaker, Vestavia, Alabama, October, 2014 Al</li>
         <li>"Art Fuels" guest artist/speaker at Indian Springs School, Indian Springs, Alabama, September, 2014 Al</li>
@@ -44,30 +44,30 @@ Template Name: Page About
         <li>Kala's Cottage - Guntersville, Alabama - 2010</li>
         <li>National Center for the Study of Civil Rights and African American Culture Exhibition - Montgomery, Alabama - June 6 - October 31, 2010</li>
         <li>1st Place Oil/Acrylic - Vestavia Art Show 2009</li>
-    </ul>
-
-
-
-
- <div class="contactForm">
-		<div class="form-text">
-			<p>If you’d like to commission Kelly to paint a portrait or fine art piece for you, or 
-        schedule a private art class, contact her for a quote.</p>
-			<h1>Contact Kelly.</h1>
-		</div>
-		<div class="form-inputs">
-			<form action="" method="post">
-				<label for="POST-name"></label><input id="POST-name" type="text" name="name" placeholder="Your Name:">
-				<label for="POST-email"></label><input id="POST-email" type="text" name="email" Placeholder="Your Email:">
-				<label for="POST-message"></label><input id="POST-message" type="text" name="message" Placeholder="Your Message:">
-				<input type="submit" value="Send" class="sendButton">
-			</form>
-		</div>
-	</div>
-
+    </ul> -->
 
 
 </section>
+
+ <div class="contactForm">
+    <div class="form-text">
+        <p>If you’d like to commission Kelly to paint a portrait or fine art piece for you, or 
+    schedule a private art class, contact her for a quote.</p>
+        <h1>Contact Kelly.</h1>
+    </div>
+    <div class="form-inputs">
+        <form action="" method="post">
+            <label for="POST-name"></label><input id="POST-name" type="text" name="name" placeholder="Your Name:">
+            <label for="POST-email"></label><input id="POST-email" type="text" name="email" Placeholder="Your Email:">
+            <label for="POST-message"></label><input id="POST-message" type="text" name="message" Placeholder="Your Message:">
+            <input type="submit" value="Send" class="sendButton">
+        </form>
+    </div>
+</div>
+
+
+
+
 
 
 
