d422cd1d978411a5b67eb5d67de237c3173757c4
Erin Kahn <erinnicolekahn@gmail.com>
2018-06-26 21:48:58 -0400

    have to have 2 footers - styling is different from home page

--- wp-content/themes/kkart/single-portfolio.php
+++ wp-content/themes/kkart/single-portfolio.php
@@ -36,10 +36,6 @@
 	?>
 
 
-
-	
-
-
 	<div class="contactForm">
 		<div class="form-text">
 			<p>Have you been wanting a new portrait of your family to hang up in your home? Do you want to improve your art skills by taking a private art class?</p>
@@ -57,4 +53,20 @@
 
 </section>
 
-<?php get_footer(); ?>
+
+<section class="footer"> <!--cant do get footer because the styling is different than the home footer -->
+
+	<div class="copyright">
+		<p>Website designed and coded by <a href="erin-kahn.com">Erin Kahn</a></p>
+	</div>
+
+	<div class="right-footer">
+		<a class="in" href="https://www.linkedin.com/in/kelly-kahn-iida-aidc-0b413214/" target="_blank"><i class="fab fa-linkedin-in"></i></a> <!-- universal so no php needed -->
+		<a class="mail" href="mailto:kellykahn61@gmail.com" target="_blank"><i class="far fa-envelope"></i></a> <!-- universal so no php needed -->
+		<a class="top" href="#"><i class="fas fa-angle-up"></i></a> <!-- universal so no php needed -->
+	</div>
+
+</section>
+
+
+
