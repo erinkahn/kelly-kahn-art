e4bab462d3d54d74b3206054eeb40e7be1d47dd2
Erin Kahn <erinnicolekahn@gmail.com>
2018-07-02 21:46:47 -0400

    burger works on single portfolio post pages
    have to have get_footer on every page

--- wp-content/themes/kkart/single-portfolio.php
+++ wp-content/themes/kkart/single-portfolio.php
@@ -54,19 +54,8 @@
 </section>
 
 
-<section class="footer"> <!--cant do get footer because the styling is different than the home footer -->
-
-	<div class="copyright">
-		<p>Website designed and coded by <a href="erin-kahn.com">Erin Kahn</a></p>
-	</div>
-
-	<div class="right-footer">
-		<a class="in" href="https://www.linkedin.com/in/kelly-kahn-iida-aidc-0b413214/" target="_blank"><i class="fab fa-linkedin-in"></i></a> <!-- universal so no php needed -->
-		<a class="mail" href="mailto:kellykahn61@gmail.com" target="_blank"><i class="far fa-envelope"></i></a> <!-- universal so no php needed -->
-		<a class="top" href="#"><i class="fas fa-angle-up"></i></a> <!-- universal so no php needed -->
-	</div>
-
-</section>
 
 
 
+
+<?php get_footer(); ?>
