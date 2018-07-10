c3bfe23633c0095a36cc3e1c37c155f17bcf43c7
Erin Kahn <erinnicolekahn@gmail.com>
2018-07-09 20:17:55 -0400

    favicon

--- wp-content/themes/kkart/archive-portfolio.php
+++ wp-content/themes/kkart/archive-portfolio.php
@@ -3,24 +3,43 @@
 <section id="archive-page" role="main">
 	
 	<div class="categories">
-		<!-- loop through all posts/portfolio categories and show all on one page -->	
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			<div class="portfolio-categories">
+	<!-- custom query for "portfolios" type - custom post type
+		loop through them
+		display them -->
+
+	<div class="portfolio-categories">
 		
-				<div class="single-post"> <!-- four images portfolios -->
-					<a class="post-link" href="<?php the_permalink(); ?>"> <!-- link to this post-->
-						<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
-						<h3><?php the_title();?></h3>
-					</a>
-					
-				</div>
+		<?php 
+			$args = array(
+				'post_type' => array('portfolio'), //get the portfolio categories
+				'post_per_page' => -1, //get all the posts is -1
+				'order' => "ASC" //puts the post types in order or you can use a WP plugin
+			);
+			$portfolioQuery = new WP_Query($args);
+			// the custom loop
+			if ($portfolioQuery->have_posts()) {
+				while($portfolioQuery->have_posts()) { //loop start
+					$portfolioQuery->the_post(); 
+					?>
 
-			</div>
+					<div class="single-post"> <!-- four images portfolios -->
+						<a class="post-link" href="<?php the_permalink(); ?>"> <!-- link to this post-->
+							<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
+							<h3><?php the_title();?></h3>
+						</a>
+						
+					</div>
 
-		<?php endwhile; endif; ?>	
+				<?php 
+				} //loop end
+				wp_reset_postdata();
+			}
+		?>
 
 	</div>
+
+</div>
 			
 		
 	
