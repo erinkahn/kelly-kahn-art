f3d9d876c02f14e843d41387877f33ce4ab89a86
Erin Kahn <erinnicolekahn@gmail.com>
2018-07-10 15:40:58 -0400

    fixed: pics dont jump on hover anymore

--- wp-content/themes/kkart/single-portfolio.php
+++ wp-content/themes/kkart/single-portfolio.php
@@ -7,34 +7,34 @@
 	<!-- ACF - advanced custom fields repeater -->
 	<!-- https://www.advancedcustomfields.com/resources/repeater/ -->
 
-	<?php
+	<div class="galleryImages">
+		<?php
 
-	// check if the repeater field has rows of data
-	if( have_rows('portfolio_image') ):
+		// check if the repeater field has rows of data
+		if( have_rows('portfolio_image') ):
 
-		// loop through the rows of data
-		while ( have_rows('portfolio_image') ) : the_row(); ?>
+			// loop through the rows of data
+			while ( have_rows('portfolio_image') ) : the_row(); ?>
 
-			<!-- display a sub field value -->
-			<div class="galleryImages">
+				<!-- display a sub field value -->
 				<div class="image-container">
 					<img src="<?php the_sub_field('portfolio_image');?>" alt="">
 					<h2> <?php the_sub_field('portfolio_title');?> </h2>
 					<p> <?php the_sub_field('portfolio_subtitle'); ?> </p>
 				</div>
-			</div>
+					
+					
+			<?php endwhile;
+
+		else :
 			
+			// no rows found
+			
+		endif;
 
-		<?php endwhile;
+		?>
 
-	else :
-
-		// no rows found
-
-	endif;
-
-	?>
-
+	</div>
 
 
 </section>
